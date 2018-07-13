3eabae5f438696263cdd038d5b14732dcc5406d2
Linn L <41192228+nettlepetal@users.noreply.github.com>
2018-07-13 17:25:46 +0200

    Player movemet
    Made folder for scripts. Added two types of player movement, change value of boolean useRotation in Player script to try both types

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //Set useRotation to preferred movement type
+    bool useRotation = false;
+    //Set speed of rotation and movement
+    int turnSpeed = 200;
+    int movementSpeed = 5;
+
+    // Use this for initialization
+    void Start () {
+
+        
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+        //Two different kinds of player movement, set in useRotation global boolean
+        if (useRotation)
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+        }
+        else
+        {
+
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+            transform.Translate(x, 0, 0);
+            transform.Translate(0, 0, z);
+        }
+        
+    }
+}
