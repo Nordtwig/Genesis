7b321b75e0a2c6c038cfb42461dabeb20920f492
Linn L <41192228+nettlepetal@users.noreply.github.com>
2018-07-16 12:34:44 +0200

    Player spawn bug fixed

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,10 @@ public class GameController : MonoBehaviour {
     //Make the game controller a static singleton
     public static GameController gameController = null;
 
-    [SerializeField] List<GameObject> pickups;
-    [SerializeField] List<GameObject> doors;
-    [SerializeField] List<GameObject> enemies;
     [SerializeField] GameObject player;
+    [SerializeField] List<GameObject> doors;
+    [SerializeField] List<GameObject> pickups;
+    [SerializeField] List<GameObject> enemies;
 
     //Awake is called before Start functions
     void Awake()
@@ -34,27 +34,54 @@ public class GameController : MonoBehaviour {
 
     void InitGame()
     {
-        //Any initialization that might be needed later
+        //Any initialization goes here
+
+        //Sets references to all objects that vill be reset upon LevelReset
+        /*player = GameObject.Find("Player").gameObject;
+
+        Transform doorsCategory = GameObject.Find("Doors").gameObject.transform;
+        foreach (Transform child in doorsCategory)
+        {
+            //Debug.Log("Found a door with name: " + child.name);
+            doors.Add(child.gameObject);
+        }
+        Transform pickupsCategory = GameObject.Find("Pickups").gameObject.transform;
+        foreach (Transform child in pickupsCategory)
+        {
+            //Debug.Log("Found a pickup with name: " + child.name);
+            pickups.Add(child.gameObject);
+        }
+        Transform enemiesCategory = GameObject.Find("Enemies").gameObject.transform;
+        foreach (Transform child in enemiesCategory)
+        {
+            //Debug.Log("Found an enemy with name: " + child.name);
+            enemies.Add(child.gameObject);
+        }*/
+
     }
 
     public void ResetLevel()
     {
-        foreach (GameObject pickup in pickups){
-            PickUp pickupScript = pickup.gameObject.GetComponent<PickUp>() as PickUp;
-            pickupScript.ResetPickup();
-        }
+        Player playerScript = player.gameObject.GetComponent("Player") as Player;
+        playerScript.ResetPlayer();
+
         foreach (GameObject door in doors)
         {
             Door doorScript = door.gameObject.GetComponent("Door") as Door;
             doorScript.ResetDoor();
         }
+
+        foreach (GameObject pickup in pickups){
+            PickUp pickupScript = pickup.gameObject.GetComponent<PickUp>() as PickUp;
+            pickupScript.ResetPickup();
+        }
+        
         foreach (GameObject enemy in enemies)
         {
             Enemy enemyScript = enemy.gameObject.GetComponent("Enemy") as Enemy;
             enemyScript.ResetEnemy();
         }
-        Player playerScript = player.gameObject.GetComponent("Player") as Player;
-        playerScript.ResetPlayer();
+        
     }
 
 }
