a10231e6def43798a20f5ad637b92112767e5c4f
Northtwig <nordqvist.gaming@gmail.com>
2018-07-17 17:16:51 +1200

    Added functionality to menu buttons.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartCredits() {
+        SceneManager.LoadScene(2); // Hardcoded, consider getting at runtime.
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+        print("Exiting game");
+    }
+}
